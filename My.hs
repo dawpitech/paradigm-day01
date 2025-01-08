699fc5c609878b0bf4ef16d8f5d4a94cd8fb0856
Antoine <me@antoinebellanger.fr>
2025-01-08 10:05:49 +0100

    feat: Task 04 -> 10

--- My.hs
+++ My.hs
@@ -1,3 +1,4 @@
+import Distribution.Simple.Utils (xargs)
 {-
 -- EPITECH PROJECT, 2025
 -- PARADIGM-DAY01
@@ -14,3 +15,26 @@ myIsNeg x = x < 0
 myAbs :: Int -> Int
 myAbs x | x >= 0 =  x
         | x <  0 = -x
+
+myMin :: Int -> Int -> Int
+myMin x y | x >= y = y
+          | x <  y = x
+
+myMax :: Int -> Int -> Int
+myMax x y | x >= y = y
+          | x <  y = x
+
+myTuple :: a -> b -> (a, b)
+myTuple a b = (a, b)
+
+myTruple :: a -> b -> c -> (a, b, c)
+myTruple a b c = (a, b, c)
+
+myFst :: (a, b) -> a
+myFst (a, b) = a
+
+mySnd :: (a, b) -> b
+mySnd (a, b) = b
+
+mySwap :: (a, b) -> (b, a)
+mySwap (a, b) = (b, a)
